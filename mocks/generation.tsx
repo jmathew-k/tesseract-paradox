ddabfcce6fbcde5813e27b414e70ca74dceca75e
Joshua Mathew <JoshuaKMathew@gmail.com>
2022-09-16 19:55:26 +0530

    edits

--- mocks/generation.tsx
+++ mocks/generation.tsx
@@ -1,23 +1,23 @@
 export const generation = [
     {
-        title: "Secure & Safe",
+        title: "Security & Privacy Built-In",
         content: "Make your design more attractive",
         label: "new",
         image: "/images/generation-pic-1.png",
-        color: "#E1D3FA",
+        color: "#28016b",
     },
     {
         title: "UX first approach",
         content: "Create, enable and use across your team",
         image: "/images/generation-pic-2.png",
-        color: "#B6DFEC",
+        color: "#07e8e4",
         delay: 300,
     },
     {
-        title: "High Performance",
+        title: "High Throuhgput",
         content: "Make your design more attractive",
         image: "/images/generation-pic-3.png",
-        color: "#CCE8BE",
+        color: "#24f3bc",
         delay: 600,
     },
 ];
